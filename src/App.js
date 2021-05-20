a2f9ef886a8477929b7bd731776215454bbe67b9
Your Name <you@example.com>
2021-05-20 13:03:10 +0100

    Created react app, added header

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import dictionaryIcon from "./dictionaryIcon.png";
+
+export default function App() {
+  return ( 
+    <div className="App">
+      <header className="App-header">
+         <img src={dictionaryIcon} className="App-logo" alt="logo"/> 
+      </header>
+    </div>
+  );
+}
+
+ 
