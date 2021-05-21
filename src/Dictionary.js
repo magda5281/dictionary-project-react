731fd94f22b57642ac1e7365fdb65a962aad7582
Your Name <you@example.com>
2021-05-21 11:35:39 +0100

    Added dictionary api integration and documentation

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,18 +1,27 @@
 import React, {useState} from "react";
+import axios from "axios";
 import "./Dictionary.css"
+
 
 export default function Dictioanry(){
 
     let [keyword, setKeyword]= useState("");
-   
 
+    function handleResponse(response){
+        console.log(response.data[0]);
+    }
+   
     function handleKeywordChange(event){
         setKeyword(event.target.value);
     }
 
     function Search(event){
         event.preventDefault()
-        alert(`Searching for ${keyword}`);
+
+        // documentation: https://dictionaryapi.dev/
+        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+       
+        axios.get(apiUrl).then(handleResponse);
     }
     return (
     <div className="Dictionary">
